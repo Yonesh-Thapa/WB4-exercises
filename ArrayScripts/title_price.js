258949caf125e1abcd102e9fd986621da4bbe2af
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-09 15:36:27 -0400

    found title by price

---
+++ ArrayScripts/title_price.js
@@ -0,0 +1,55 @@
+let courses = [
+  {
+    CourseId: "PROG100",
+    Title: "Introduction to HTML/CSS/Git",
+    Location: "Classroom 7",
+    StartDate: "09/08/22",
+    Fee: "100.00",
+  },
+  {
+    CourseId: "PROG200",
+    Title: "Introduction to JavaScript",
+    Location: "Classroom 9",
+    StartDate: "11/22/22",
+    Fee: "350.00",
+  },
+  {
+    CourseId: "PROG300",
+    Title: "Introduction to Java",
+    Location: "Classroom 1",
+    StartDate: "01/09/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROG400",
+    Title: "Introduction to SQL and Databases",
+    Location: "Classroom 7",
+    StartDate: "03/16/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROJ500",
+    Title: "Introduction to Angular",
+    Location: "Classroom 1",
+    StartDate: "04/25/23",
+    Fee: "50.00",
+  },
+];
+function displayTitle(course) {
+  console.log(`
+Title: ${course.Title}
+Fee: ${course.Fee}
+`);
+}
+
+function priceTitle(id) {
+  let numOfCourse = courses.length;
+  for (i = 0; i < numOfCourse; i++) {
+    course = courses[i];
+    if (course.Fee <= 50) {
+      displayTitle(course);
+    }
+  }
+}
+
+priceTitle(50);
